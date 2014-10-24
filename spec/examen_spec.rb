c61b3bcc957406506b79d8efe7671aa56e3409a1
Victor Hernandez Perez <alu0100697032.edu.es>
2014-10-24 12:43:58 +0000

    Corregido examen_spec.rb

--- spec/examen_spec.rb
+++ spec/examen_spec.rb
@@ -18,22 +18,22 @@ describe Examen do
     
     describe "Getter de enunciado" do
       it "devuelve el valor de p1.enunciado" do
-        @p1.enunciado.should eq ("Enunciado")
+        @p1.enunciado == "Enunciado"
       end
   end
   
   describe "Getter de respuestas" do  
       it "devuelve el valor de p1.respuesta_i" do
-        @p1.respuesta_a.should eq ("a")
-        @p1.respuesta_b.should eq ("b")
-        @p1.respuesta_c.should eq ("c")
-        @p1.respuesta_d.should eq ("d")
+        @p1.respuesta_a == "a"
+        @p1.respuesta_b == "b"
+        @p1.respuesta_c == "c"
+        @p1.respuesta_d == "d"
       end
   end
   
   describe "to_s" do
       it "to_s devuelve una cadena con la preguntas y las respuestas para poder mostrar por consola" do
-        @p1.to_s.should eq ("Enunciado \n a) a \n b) b \n c) c \n d d ")
+        @p1.to_s == "Enunciado \n a) a \n b) b \n c) c \n d) d"
       end
   end
 end
