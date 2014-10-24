71bcd4aa7aef0f8c0390a8ade174add6baa52400
Victor Hernandez Perez <alu0100697032.edu.es>
2014-10-24 11:11:04 +0000

    Expectativa creada para comprobar que existe una pregunta y opciones de respuesta

--- spec/examen_spec.rb
+++ spec/examen_spec.rb
@@ -1,1 +1,15 @@
 require 'examen'
+
+describe Examen do
+    describe "La pregunta es coherente" do
+        it "Existe enunciado" do
+            @p1.enunciado != nil
+        end
+        it "Existen respuestas" do
+            @p1.respuesta_a != nil
+            @p1.respuesta_b != nil
+            @p1.respuesta_c != nil
+            @p1.respuesta_d != nil
+        end
+    end
+end
