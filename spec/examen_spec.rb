d909268b26eb25bb85310dfd62f784e5c9e686f1
alu0100837353 <alu0100837353@ull.edu.es>
2014-10-24 12:19:10 +0000

    Corregido el error en examen_spec.rb

--- spec/examen_spec.rb
+++ spec/examen_spec.rb
@@ -18,16 +18,16 @@ describe Examen do
     
     describe "Getter de enunciado" do
       it "getenum devuelve el valor de p1.enunciado" do
-        @p1.getenun.should eq ("Enunciado")
+        @p1.enunciado.should eq ("Enunciado")
       end
   end
   
   describe "Getter de respuestas" do  
       it "getres_i devuelve el valor de p1.respuesta_i" do
-        @p1.getres_a.should eq ("a")
-        @p1.getres_b.should eq ("b")
-        @p1.getres_c.should eq ("c")
-        @p1.getres_d.should eq ("d")
+        @p1.respuesta_a.should eq ("a")
+        @p1.respuesta_b.should eq ("b")
+        @p1.respuesta_c.should eq ("c")
+        @p1.respuesta_d.should eq ("d")
       end
   end
   
