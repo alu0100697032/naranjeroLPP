1aec1d21e5bc3825763fec50a7b7fd9c93cc4402
Victor Hernandez Perez <alu0100697032.edu.es>
2014-12-15 12:49:39 +0000

    Cumplida expectativa para comprobar si el árbol esta o no vivo

--- lib/examen/naranjero.rb
+++ lib/examen/naranjero.rb
@@ -1,15 +1,25 @@
 class Naranjero
-    attr_reader :altura, :edad, :contador
+    attr_reader :altura, :edad, :contador, :arbolVivo
     
     def initialize(altura, edad, contador)
         @altura = altura
         @edad = edad
         @contador = contador
+        if(@edad > 100)
+            @arbolVivo = false
+        else 
+            @albolVivo = true
+       end
     end
     
     def uno_mas
-        @altura = @altura + 0.1
-        @edad = @edad + 1
-        @contador = @edad/5
+        @edad = @edad +1
+        if(@edad > 100)
+            @arbolVivo = false
+            @contador = 0
+        else
+            @altura = @altura + 0.1
+            @contador = @edad/5
+        end
     end
 end
