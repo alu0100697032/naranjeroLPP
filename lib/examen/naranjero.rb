858607d79c94dd56c2478baae746a18b2e65b9b8
Victor Hernandez Perez <alu0100697032.edu.es>
2014-12-15 12:28:04 +0000

    Cumplida la expectativa anterior

--- lib/examen/naranjero.rb
+++ lib/examen/naranjero.rb
@@ -6,4 +6,10 @@ class Naranjero
         @edad = edad
         @contador = contador
     end
+    
+    def uno_mas
+        @altura = @altura + 0.1
+        @edad = @edad + 1
+        @contador = @edad/5
+    end
 end
